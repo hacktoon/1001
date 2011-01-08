53b447356bf1bd9b168f036d2d736a24b142466c
Sipmann <sipmann@gmail.com>
2011-01-09 04:28:40 +0800

    Adicionado o bozosort.php

---
+++ sorting/bozosort/bozosort.php
@@ -0,0 +1,42 @@
+<?php
+	
+/*
+Bozosort (a.k.a Bogosort)
+Autor: 
+	Bozo
+Tipo: 
+	sorting
+Descrição: 
+	Ordena um array a fim de ficar em ordem crescente.
+Complexidade:
+	Pior caso: Infinito
+	Melhor caso: ?
+Dificuldade:
+	facil
+Referências:
+	http://pt.wikipedia.org/wiki/Bogosort
+*/
+
+	function bogo($valores) {
+		$ordenado = false;
+		$certo = $valores;
+		sort($certo);
+		
+		$i = 0;
+		
+		while(!$ordenado) {
+			shuffle($valores);
+			if( $valores == $certo) {
+				$ordenado = true;
+			}
+			$i++;
+			echo "Tentativa ".$i." <br>";
+			print_r(($ordenado==true)? $valores: "");
+		}
+		
+	}
+	
+	// Passando um array com valores a serem ordenados
+	bogo(array(3,5,2,1));
+	
+?>
