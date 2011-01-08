f0ca4eda2e1dbd78fa62a27ceec98ade8147ccb1
Karlisson Bezerra <theanimage@gmail.com>
2011-01-08 12:43:49 -0300

    Added first crypto algorithm

---
+++ crypto/rot13.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3.1
+
+"""
+ROT13
+Autor: Fernando Medeiros <fekomedeiros - at - gmail.com>
+Descrição: 
+    Implementação do algoritmo ROT-13, ou "Rotate By 13".
+    É um procedimento simples mas eficaz para garantir que textos eletrônicos 
+    não sejam lidos por distração ou acidente.
+    Util para proteger mensagens que talvez o leitor não queira ler. 
+    Exemplo, "spoilers" sobre determinado assunto em Fóruns ou listas de discussão.
+Complexidade:
+    Muito fácil
+Referências:
+    http://pt.wikipedia.org/wiki/ROT13
+"""
+
+def rot13(text):
+    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    rotated = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+    r13 = "".maketrans(alpha, rotated)
+    return text.translate(r13)
+
+#Exemplos de uso:
+
+print(rot13("Agora estou usando ROT-13!"))
+#Exibe a mensagem: Ntben rfgbh hfnaqb EBG-13!
+
+print(rot13("Ntben rfgbh hfnaqb EBG-13!"))
+#Exibe a mensagem: Agora estou usando ROT-13!
+#Note que a mesma função é usada para codificar e decodificar o texto.
