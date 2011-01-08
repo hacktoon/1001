fbdfe338a66b48c82318d537e5b73ff4e693d015
Karlisson Bezerra <theanimage@gmail.com>
2011-01-08 12:50:36 -0300

    Minor fixes

--- sequence/token.php
+++ sequence/token.php
@@ -3,11 +3,12 @@
 /*
 Token
 Autor: Sérgio Mandrake <sergio_mandrake@yahoo.com.br>
-Tipo: ?
+Tipo: sequence
 Descrição: Gera um token aleatório
 Complexidade:
     Pior caso: ?
     Melhor caso: ?
+Dificuldade: fácil
 */
 
 function token($comprimento = 6) {
@@ -15,7 +16,7 @@ function token($comprimento = 6) {
     $totalCarac = strlen($caracteres) -1;
     $resultado = '';
     for ($i = 0; $i < $comprimento; $i++) {
-        $resultado .= $caracteres[mt_rand($i, $totalCarac)];
+        $resultado .= $caracteres[mt_rand(0, $totalCarac)];
     }
 
     return $resultado;
