2dd0451834b0d16ffe672faefd60f872fe02a860
Karlisson Bezerra <theanimage@gmail.com>
2011-01-08 10:05:12 -0300

    Added classic media algorithm

---
+++ math/media.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+"""
+Cálculo da média ponderada
+Autor: Desconhecido
+Tipo: Matemática
+Descrição: Calcula a média ponderada - é um algoritmo
+    comum em qualquer curso de introdução à programação,
+    que pode variar de acordo com os pesos.
+Complexidade:  
+    Pior caso: O(1)
+    Melhor caso: O(1)
+"""
+
+import math
+
+def media(n1, n2, n3):
+    p1, p2, p3 = 4, 5, 6
+    return (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3)
+
+print media(7.0, 8.0, 10.0)
