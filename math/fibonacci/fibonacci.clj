b7f9d6d8e1fdbedb086d6bcf3be0c73556dfc309
victal <guilhermevictal@gmail.com>
2011-01-10 03:40:57 +0800

    adicionando algoritmo para cálculo da sequencia de fibonacci

---
+++ math/fibonacci/fibonacci.clj
@@ -0,0 +1,27 @@
+(comment 
+Cálculo da sequência de Fibonacci
+Autor 
+  ? 
+Colaborador
+Guilherme Victal (guilhermevictal em gmail.com) 
+Tipo
+  math
+Descrição 
+  Cálculo do n-ésimo valor da sequência de fibonacci recursivamente,
+  a partir da definição [a[n]=a[n-1] + a[n-2], a[0]=a[1]=1]
+  Na prática é muito lento mesmo para n não muito grande;
+Complexidade
+  ? 
+Dificuldade
+  facil
+)
+
+
+(defn fibonacci [n]
+  (if (< n 2)
+    1
+    (+ (fibonacci (- n 1)) (fibonacci (- n 2)))))
+
+
+(println (map fibonacci (range 1 20)))
+
