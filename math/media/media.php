e46859eb7b0f4578d1172cb9910a770065fea95e
Sipmann <sipmann@gmail.com>
2011-01-09 17:24:35 -0200

    Added php version of media

---
+++ math/media/media.php
@@ -0,0 +1,29 @@
+<?php 
+
+/*
+Cálculo da média ponderada
+Autor:
+    ?
+Colaborador:
+    Karlisson Bezerra
+Tipo:
+    math
+Descrição:
+    Calcula a média ponderada - é um algoritmo
+    comum em qualquer curso de introdução à programação,
+    que pode variar de acordo com os pesos.
+Complexidade:  
+    O(1)
+Dificuldade:
+    facil
+*/
+
+function media($n1, $n2, $n3) {
+    $p1 = 4;
+	$p2 = 5;
+	$p3 = 6;
+    return ($n1 * $p1 + $n2 * $p2 + $n3 * $p3) / ($p1 + $p2 + $p3);
+}
+
+print media(7.0, 8.0, 10.0);
+?>
