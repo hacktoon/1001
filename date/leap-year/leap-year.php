95628b54ca2b04d75348a24687f0db1bb6947b47
Juan Lopes <juanplopes@gmail.com>
2011-01-09 02:02:20 -0200

    fixing bug in leap year calculation (gregorian calendar)

--- date/leap-year/leap-year.php
+++ date/leap-year/leap-year.php
@@ -23,7 +23,7 @@ function bissexto($numAnos = 4) {
     
     $anos = array();
     for ($i = $anoAtual; $i <= $anoFinal; $i++) {
-        if (($i % 4) == 0) {
+        if ($i % 4 == 0 && ($i % 100 != 0 || $i % 400 == 0)) {
             $anos[] = $i;
         }
     }
