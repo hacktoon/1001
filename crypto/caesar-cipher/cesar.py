580b95d0cd86c1af1edffd3b1022be428586a124
InFog <junior@casoft.info>
2011-01-14 19:53:50 -0200

    Adicionado algoritmo com a Cifra de César.

---
+++ crypto/caesar-cipher/cesar.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+Cesar Cipher (Cifra de César)
+Autor:
+    César
+Colaborador:
+    Apu, Sigano, InFog, Paulo, Doug, ExHora <gccsd@lista.gccsd.com.br>
+Tipo:
+    crypto
+Descrição:
+    Este algoritmo implementa a Cifra de César
+    Este algoritmo foi implementado em um Dojo do Grupo de Compartilhamento do
+    Conhecimento Santos Dumont <http://gccsd.com.br>
+Complexidade:  
+    ?
+Dificuldade:
+    facil
+Referências:
+    http://pt.wikipedia.org/wiki/Cifra_de_C%C3%A9sar
+Licenca:
+    GPL
+"""
+
+__authors__ = (
+    "Apu",
+    "Sigano",
+    "InFog",
+    "Paulo",
+    "Doug",
+    "ExHora"
+)
+
+import string
+
+class Cesar(object): 
+    
+    def __init__(self):
+        self.INICIO = 65
+        self.FIM = 90
+        self.ESPACO = 32
+    
+    def crypt(self, entrada = "", chave = 0):
+        saida = ""
+        entrada = entrada.upper()
+
+        for letra in entrada:
+            valor = ord(letra)
+            
+            if (not valor == self.ESPACO):
+                valor += chave
+                if (valor > self.FIM):
+                    valor -= 26
+            saida += chr(valor)
+            
+        return saida
+    
+    def decrypt(self, entrada = "", chave = 0):
+        saida = ""
+        entrada = entrada.upper()
+        
+        for letra in entrada:
+            valor = ord(letra)
+            
+            if (not valor == self.ESPACO):
+                valor -= chave
+                if (valor < self.INICIO):
+                    valor += 26
+            saida += chr(valor)
+        
+        return saida
