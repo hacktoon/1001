3a6f6416205a488cc3b59f14f5cdc0caf87e84ab
Karlisson Bezerra <theanimage@gmail.com>
2011-06-21 21:30:30 -0300

    Adicionando o algoritmo mais simples de todos...Hello World.

---
+++ misc/helloworld.py
@@ -0,0 +1,17 @@
+"""
+Helloworld
+Autor:
+    ?
+Colaborador:
+    Karlisson - contato@nerdson.com
+Tipo:
+    misc
+Descrição:
+    Imprime a string Hello world
+Complexidade:  
+    O(1)
+Dificuldade:
+    facil
+"""
+
+print "Hello world"
