d2ed018383a7be4f4e9f10ce0819079cded6b6da
Ricardo Bittencourt <bluepenguin@gmail.com>
2011-08-06 07:13:51 -0700

    Reorganizing math

---
+++ algorithms/math/number-theory/euclid.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+
+"""
+Algoritmo de Euclides
+Autor:
+    Euclides de Alexandria
+Colaborador:
+    Liquen 
+Tipo:
+    number-theory
+Descrição:
+    Algoritmo de Euclides em sua forma moderna. Computa o máximo
+    divisor comum (MDC) entre dois números inteiros. Parte do princípio de
+    que o MDC não muda se o menor número for subtraído do maior. [1] [2]
+Complexidade:
+    O(n^2), onde n é o número de dígitos da entrada. [3]
+    O número de passos é no máximo
+        log(max(a, b) * sqrt(5)) / log(phi),
+    onde phi = 1.618... é a proporção áurea. [3]
+Dificuldade:
+    facil
+Referências:
+    [1] http://en.wikipedia.org/wiki/Euclidean_algorithm
+    [2] http://pt.wikipedia.org/wiki/Algoritmo_de_Euclides
+    [3] http://mathworld.wolfram.com/EuclideanAlgorithm.html
+"""
+
+def euclides(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+print(euclides(1071, 462))
